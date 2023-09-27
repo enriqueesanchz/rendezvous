791f63ee2a4a8cce65477eb6410299baf909fde0
enriqueesanchz <enrsancar1@alum.us.es>
2023-09-28 00:10:09 +0200

    Add updating a peer

--- routes.py
+++ routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Body, Request, Response, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from typing import List
 
-from models import Namespace, NamespaceUpdate
+from models import Namespace, NamespaceUpdate, PeerUpdate
 
 router = APIRouter()
 
@@ -51,6 +51,38 @@ def update_namespace(name: str, request: Request, namespace: NamespaceUpdate = B
         return existing_namespace
 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Namespace with name {name} not found")
+
+@router.put("/{name}/{username}", response_description="Update a peer", response_model=Namespace)
+def update_peer(name: str, username: str, request: Request, peer: PeerUpdate = Body(...)):
+    peer = {k: v for k, v in peer.dict().items() if v is not None}
+
+    existing_peer = request.app.database["namespaces"].find_one(
+        { "peers.username": username }, {"peers": { "$elemMatch": { "username": username } } }
+    )
+
+    if len(peer) >= 1:
+        if ("port" in peer.keys()):
+            update_result = request.app.database["namespaces"].update_one(
+                {"name": name, "peers.username": username}, {"$set": {"peers.$.port":  peer["port"]}}
+            )
+
+            if update_result.modified_count == 0:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Namespace with name {name} and user {username} not found")
+        
+        if ("ip" in peer.keys()):
+            update_result = request.app.database["namespaces"].update_one(
+                    {"name": name, "peers.username": username}, {"$set": {"peers.$.ip":  peer["ip"]}}
+            )
+
+            if update_result.modified_count == 0:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Namespace with name {name} and user {username} not found")
+
+    if (
+        existing_namespace := request.app.database["namespaces"].find_one({"name": name, "peers.username": username})
+    ) is not None:
+        return existing_namespace
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Namespace with name {name} and user {username} not found")
 
 @router.delete("/{name}", response_description="Delete a namespace")
 def delete_namespace(name: str, request: Request, response: Response):
